b480d10035931c6e23adb5a1162fc8a40e77f1d2
Erik Z <molagg92@gmail.com>
2023-07-14 13:53:05 -0700

    fill out namespace usage, and make a bread and a pastry class

---
+++ Bakery.Solutions/Models/Bread.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Models
+{
+  public class Bread
+  {
+    
+  }
+}
--- Bakery.Solutions/Project.cs
+++ Bakery.Solutions/Project.cs
@@ -1,5 +1,5 @@
 using System;
-
+using Bakery.Models;
 using System.Collections.Generic;
 
 namespace ProjectName
