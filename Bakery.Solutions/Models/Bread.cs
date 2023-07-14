aaa9843de34bac470d282e17d91a823961edaef0
Erik Z <molagg92@gmail.com>
2023-07-14 14:11:17 -0700

    give my first passing green in the testing phase

--- Bakery.Solutions/Models/Bread.cs
+++ Bakery.Solutions/Models/Bread.cs
@@ -3,11 +3,20 @@ using System;
 
 namespace Bakery.Models
 {
-  public class Bread
+public class Bread
   {
+    public int Price = 5;
+    public int LoafQuantity { get; set; }
+
     public Bread(int Quantity)
     {
       LoafQuantity = Quantity;
+    }
+
+    public int BreadOrder()
+    {
+
+      return ((LoafQuantity / 3) * 2 + (LoafQuantity % 3)) * Price;
     }
   }
 }
